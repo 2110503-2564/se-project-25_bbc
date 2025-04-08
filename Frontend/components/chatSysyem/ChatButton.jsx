4a7b822bf6546fefe809c4f09f180ac0b1a5c81e
Piyakorn Sutreerojthakool <Piyakorn2112@hotmail.com>
2025-04-08 14:43:40 +0700

    chat context

---
+++ Frontend/components/chatSysyem/ChatButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ChatButton = () => {
+  return (
+    <div
+      className='hdcard_white align_item_center'
+      style={{position:"fixed", bottom:"20px", right:"20px", width:"45px", height:"45px", borderRadius:"40px"}}
+    >
+      <img src='/icons/CHAT LOGO.svg' style={{width:"20px"}}/>
+    </div>
+  )
+}
+
+export default ChatButton
