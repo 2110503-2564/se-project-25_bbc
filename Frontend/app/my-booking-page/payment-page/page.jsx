9c92db72d2386d99567fee16b26724b5a085c8a9
Piyakorn Sutreerojthakool <Piyakorn2112@hotmail.com>
2025-04-24 22:30:16 +0700

    payment page

--- Frontend/app/my-booking-page/payment-page/page.jsx
+++ Frontend/app/my-booking-page/payment-page/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { PaymentFrom } from '@components/forms/PaymentForm'
 const page = () => {
   return (
-    <div style={{marginBottom:"200px"}} className='bg-gradient-to-b from-blue-500 to-white min-h-screen flex p-4'>
+    <div style={{ marginBottom: "200px" }} className="bg-gradient-to-b from-blue-500 to-white min-h-screen flex items-center justify-center p-4">
       <PaymentFrom />
     </div>
   )
--- Frontend/components/forms/PaymentForm.jsx
+++ Frontend/components/forms/PaymentForm.jsx
@@ -5,25 +5,31 @@ import { use, useEffect, useState } from 'react';
 import { SuccessDialog } from '@components/cards/SuccessDialog';
 
 export const PaymentFrom = ({
-  hotel_id,
-  room_id,
-  total_price,
-  status = "pending",
-  roomCapacity,
-  hotelName,
-  room
-}) => {
-  
+
+}={}) => {
+  const [file, setFile] = useState(null);
 
   return (  
     <div className="w-full h-screen p-6 hdcard_white max-w-[500px] mt-20">
       <h2 className="text-2xl font-bold main_text mb-4">Payment</h2>
 
       <p className="text-sm sub_text mb-4">
-        Booking cannot exceed <span className="font-semibold text-black">4 days</span>
+        Pay via mobile banking
       </p>
 
-      <div style={{position:"relative", overflow:"hidden", height:"450px"}} className="rounded-lg card_bg2 p-6 mb-4 bg-white text-gray-600"></div>
+      <div style={{position:"relative", overflow:"hidden", height:"450px"}} className="rounded-lg card_bg2 p-6 mb-4 bg-white text-gray-600">
+        Qr Code Here
+      </div>
+      <div className='card_bg2 p-2 rounded-lg mt-3 mb-3'>
+          <div className=' main_text font-semibold'>Upload receipt</div>
+            <input type="file" name="photo" onChange={(e) => {setFile(e.target.files[0]);}} />
+          </div>
+      <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 w-full py-2 hover:bg-blue-600 transition-all text-center  rounded-md"
+        >
+          <p>Confirm</p>
+        </button>
       
      
     </div>
